d61c7de0981be0c0e07129001306be421245997e
MagnusTiberius <bgonzales20001@gmail.com>
2018-05-05 10:25:45 -0500

    Init

---
+++ src/test/java/fractalfintech/orderbook/OrderbookApplicationTests.java
@@ -0,0 +1,16 @@
+package fractalfintech.orderbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
